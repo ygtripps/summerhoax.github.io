859d83cf50ec7d3e75b64f5c6c848482c723a7db
Lunaversity <austin@crident.com>
2019-05-30 21:40:46 -0700

    introduce head for meta tags & styles

---
+++ includes/views/head.php
@@ -0,0 +1,22 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+<meta content="summerhosts, summerhoaxes, summerhoax, summerhoaxes, shit hosts, shit, hosts, host, shit host" name="keywords">
+<meta content="Welcome to SummerHoax." name="description">
+<meta content="index,follow,noodp,noydir" name="robots">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://summerhoax.com">
+<meta property="og:title" content="SummerHoax">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://summerhoax.com/">
+<meta property="twitter:title" content="SummerHoax">
+<meta property="twitter:description" content="Free and open source project exposing shit hosts, Summerhosts.">
+<meta property="twitter:image" content="https://summerhoax.com/assets/logo.png">
+<meta property="og:site_name" content="SummerHoax — Homepage">
+<meta property="og:description" content="We are a free and open source project exposing shit hosts for what they are.">
+<meta property="og:locale" content="en-US">
+<meta property="og:image" content="https://summerhoax.com/assets/logo.png">
+
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/main.css"> <!-- TODO: Add integrity Check -->

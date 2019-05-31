491d8a53eaaff08d0cea3a70ef433fd2ad996b25
Lunaversity <austin@crident.com>
2019-05-30 21:56:15 -0700

    index.html -> index.php, added views, beautified

---
+++ vault/index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once('../includes/views/head.php'); ?>
+    <title>Home | Summer Hoax Club!</title>
+</head>
+
+<body>
+<div class="container">
+    <?php include_once('../includes/views/navbar.php'); ?>
+    <h1 class="roundedbox">The Vault</h1>
+    <p style="text-align: center">Welcome to The Vault. Below, you will find a gallery of hosting providers, and a specifically tailored page for each entry shown below.</p>
+    <div class="card-columns">
+        <div class="card">
+            <a href="#">
+                <img src="assets/webrex/aaron.png" class="card-img-top" alt="Logo of WebRex">
+                <div class="card-body">
+                    <h5 class="card-title">Aaron Ward</h5>
+            </a>
+            <p class="card-text">Aaron Ward, owner of Webrex Hosting, has been banned from mc-market for asking a fourteen-year-old for nudes. He made an attempt to ban evade, which failed, just like his many other attempts: including, Webrex Hosting, which has been down for multiple days. He has been losing clients faster than Voxility creates hosts, and is known to be using suicidal thoughts, depression, and a feeling of a failed investment as an excuse to keep his host open. While the validity of his depression is not verified, it is clear he is using it as an excuse to continue to create new discord and forum accounts in order to bother members of our communities. Click "Read More" to view proof.</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated May 21, 2019</small>
+            <!--<span style="float:right;">
+                <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+            </span> -->
+            <span style="float:right;">
+                <a href="https://summerhoax.com/vault/aaron/index.html">Read More</a>
+            </span>
+        </div>
+    </div>
+    <div class="card">
+        <a href="#">
+            <img src="assets/webrex/logo.png" class="card-img-top" alt="Logo of WebrexHosting">
+            <div class="card-body">
+                <h5 class="card-title">Webrex Hosting - Dedifire</h5>
+        </a>
+        <p class="card-text">Owner has been banned on MCM for pedophilia. Same owner as Dedifire.co.uk, which got taken down by DMCA since it was a fake ripoff of the real DediFire company. Staff were leakers and owner didn't care.</p>
+    </div>
+    <div class="card-footer">
+        <small class="text-muted">Last updated May 2nd, 2019</small>
+        <span style="float:right;">
+                <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+            </span>
+    </div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/veritor/atlias-cubiknode-logo.png" class="card-img-top" alt="Combined company logos under Veritor.">
+        <div class="card-body">
+            <h5 class="card-title">Veritor - CubikNode & Atlias</h5>
+    </a>
+    <p class="card-text">A well-known scam, CubikNode transformed from a once thriving host to a 2-star 'poor' rating on trustpilot because of well over a dozen reports of scamming. The CEO has vanished and has left his clients with nothing.</p>
+    <p class="card-text">Atlias ticks all the marks for being on the SummerHoax watchlist. Run under the same company managing the well-known scam Cubiknode, Atlias.io is also using by the same support team, who have not been active in over a month.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small>
+    <span style="float:right;">
+                <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+            </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/fortresshost/fortresshost-logo.png" class="card-img-top" alt="Logo of FortressHost">
+        <div class="card-body">
+            <h5 class="card-title">FortressHost</h5>
+    </a>
+    <p class="card-text">A failure, a scam, a meme that had long overstayed its welcome - Fortress Host was all of these things. Favorite practices included: exploiting free hardware, promising false promises, and offering 'alternative' refunds.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small>
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/itzawesome/itzawesome.png" class="card-img-top" alt="Image representing ItzAwesome">
+        <div class="card-body">
+            <h5 class="card-title">ItzAwesome</h5>
+    </a>
+    <p class="card-text">This unintelligent leaker paid for a resource using his leak name (awecrift). While attempting to crack the resource, he removed one too many lines and resulted in nuking one of his nodes. 'I won't leak anymore'[sic]</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small>
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/voxtility/voxtility.png" class="card-img-top" alt="Avatar of Voxtility from MCM">
+        <div class="card-body">
+            <h5 class="card-title">Voxtility</h5>
+    </a>
+    <p class="card-text">This superhero was born with the power to recreate anything after it receives bad rep.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small>
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/skybomb/skybomb.png" class="card-img-top" alt="Image to represent SkyBomb (Credit to Rob Hopkins)">
+        <div class="card-body">
+            <h5 class="card-title">SkyBomb</h5>
+    </a>
+    <p class="card-text">Not only was this 11yo able to create 3 new hosts without breaking a sweat, he fell for a 'remove all files' prank and deleted his entire node. He has also made multiple death threats, hacking threats and been exposed on MC-Market.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A | Image Credit to Rob Hopkins</small>
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/enteronworld/enteronworld.png" class="card-img-top" alt="Logo of Enteron World">
+        <div class="card-body">
+            <h5 class="card-title">Enteron World | FCCAGut</h5>
+    </a>
+    <p class="card-text">Ran by the famous lunatic. Makes a load of bold claims saying that they will rape the industry (yes, literally) when they don't even have a very basic proper host setup. They also hired haxxors to haxx criticism.</p>
+    <p class="card-text">This ingenius hobo was able to create an entirely new, much more shittier, view on the hosting industry as 'real estate'. Since then, he has become a giant meme and is likely on the FBI Watch List for 'Lunatic Men'.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small>
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="https://summerhoax.com/vault/icx">
+        <img src="https://icxrealty.com/wp-content/uploads/2013/06/tlogo.png" class="card-img-top" alt="Logo of InterConnX">
+        <div class="card-body">
+            <h5 class="card-title">InterConnX | ICXRealty</h5>
+    </a>
+    <p class="card-text">Harnessing the power of a MC-Market system admin to make their datacenter great again! Also threatened to take down SummerHoax on their thread. Now featuring underage staff.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated April 29, 2019</small>
+    <span style="float:right;">
+            <a href="https://summerhoax.com/vault/icx">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/fragbytehosting/fragbytehosting-logo.png" class="card-img-top" alt="Logo of FragbyteHosting">
+        <div class="card-body">
+            <h5 class="card-title">FragbyteHosting</h5>
+    </a>
+    <p class="card-text">Abusing Google Cloud Free tier. A 'host' running off of a single core, 2 GB VPS - took 2 minutes to start a 1.10.2 server</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small> <!-- Make sure you Update! -->
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/mcshostingltd/mcshostingltd-logo.png" class="card-img-top" alt="Logo of MCSHosting LTD">
+        <div class="card-body">
+            <h5 class="card-title">MCS Hosting LTD</h5>
+    </a>
+    <p class="card-text">Ddosing competitors (Deploys.io - then DeluxeNode). Nicke also lied to me that it was false accusations in an attempt to get his horrible company removed from the site. As soon as he saw the proof, he cried 'It has been paid for'.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small>
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/bjncloud/bjncloud-logo.png" class="card-img-top" alt="Logo of BJNCloud Hosting">
+        <div class="card-body">
+            <h5 class="card-title">BJNCloud Hosting</h5>
+    </a>
+    <p class="card-text">Host ran by a notirious active leaker JesusX0 (a.k.a NiñoRataPistolita on BlackShitMC). They are also responsible for making a clone of an old host named Dedifire and claim that the host was sold to them.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated N/A</small>
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/server642/server642-logo.png" class="card-img-top" alt="Logo of Server642 LLC">
+        <div class="card-body">
+            <h5 class="card-title">Server642 LLC</h5>
+    </a>
+    <p class="card-text">Using nulled WHMCS and doubled down on it saying he does not see a point in spending money on something he can get for free. Super shady Discord, selling stuffs like Spotify accounts to claiming that they have a $3000 DDOS Protection (which we highly doubt they have - they can't even spend $15 on a WHMCS license).</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated April 28, 2019</small> <!-- Make sure you Update! -->
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+<div class="card">
+    <a href="#">
+        <img src="assets/termsgames/termsgames-logo.png" class="card-img-top" alt="Logo of TermsGames">
+        <div class="card-body">
+            <h5 class="card-title">TermsGames</h5>
+    </a>
+    <p class="card-text">Sold machines to people and when he ran out of money to keep them up, he took them offline without allowing backups to be made. Has been caught selling dodgy windows keys and trying to resell nulled themes. Banned people on his discord for not giving him $15 on request. Scammed people by offering setup services and ghosting after getting money. When asked for money back he said he didn't care. Owner of Venelium Hosting Ltd (11102714) and Voroid Unturned Community.</p>
+</div>
+<div class="card-footer">
+    <small class="text-muted">Last updated May 25, 2019</small> <!-- Make sure you Update! -->
+    <span style="float:right;">
+            <a tabindex="0" data-toggle="popover" data-trigger="focus" title="Information" data-content="Sorry, we're currently developing this page!">Read More</a>
+        </span>
+</div>
+</div>
+
+<?php require_once('../includes/views/scripts.php'); ?>
+</body>
+
+</html>
